74cce5acf404b374c6da209d3b69100720ed7cb1
Lenhat14810 <lengocminhnhat01@gmail.com>
2026-04-14 13:34:34 +0700

    feat: add auth services

---
+++ Frontend/src/services/auth-services.js
@@ -0,0 +1,10 @@
+import api from '../utils/api'
+
+export const loginAPI = (credentials) =>
+  api.post('auth/login', credentials)
+
+export const getMeAPI = () =>
+  api.get('auth/me')
+
+export const logoutAPI = () =>
+  api.post('auth/logout')
